eae11ab7b6b3f5eadb116bc55782a0e56b05aa9a
Hallison Melo <hallison.pm@gmail.com>
2019-09-16 13:46:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# accents
+
+> Remove all accents lightly.
+
+## How to Start
+
+
+1. Install with `npm i --save accents`
+2. Import the libary:
+````javascript
+import accents from 'accents';
+
+console.log(accents("Wáter"))
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = function (txt) {
+	return txt.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a')
+		.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e')
+		.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i')
+		.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o')
+		.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u')
+		.replace(new RegExp('[Ç]', 'gi'), 'c').
+}
