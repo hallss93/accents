0dc92507c1e53f2711c581eb3e72c4f828fd61c7
Hallison <hallison.pm@gmail.com>
2022-03-25 16:10:57 -0300

    u trema

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = function (txt) {
 		.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e')
 		.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i')
 		.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o')
-		.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u')
+		.replace(new RegExp('[ÚÙÛÜ]', 'gi'), 'u')
 		.replace(new RegExp('[Ç]', 'gi'), 'c')
 }
